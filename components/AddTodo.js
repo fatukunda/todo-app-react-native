1e7876f4dd3db18261e0fc5e5e753d08484f2564
fatukunda <fatukunda@gmail.com>
2020-05-06 17:18:09 +0300

    Add a modal

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
-import { View, Button, StyleSheet, TextInput } from "react-native";
+import { View, Button, StyleSheet, TextInput, Modal } from "react-native";
 
-const AddTodo = ({ addTodoItem }) => {
+const AddTodo = ({ addTodoItem, isVisible, setModalVisible }) => {
   const [todoText, setTodoText] = useState("");
   const changeTextHadler = (text) => {
     setTodoText(text);
   };
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.todoInput}
-        value={todoText}
-        onChangeText={changeTextHadler}
-      />
-      <View style={styles.actionButton}>
-        <View style={styles.btn}>
-          <Button title="Add Todo" onPress={() => addTodoItem(todoText)} />
-        </View>
-        <View style={styles.btn}>
-          <Button title="CANCEL" color="red" />
+    <Modal visible={isVisible} animationType="slide">
+      <View style={styles.container}>
+        <TextInput
+          style={styles.todoInput}
+          value={todoText}
+          onChangeText={changeTextHadler}
+        />
+        <View style={styles.actionButton}>
+          <View style={styles.btn}>
+            <Button
+              title="Add Todo"
+              onPress={() => addTodoItem(todoText, setTodoText)}
+            />
+          </View>
+          <View style={styles.btn}>
+            <Button
+              title="CANCEL"
+              color="red"
+              onPress={() => setModalVisible(false)}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </Modal>
   );
 };
 export default AddTodo;
@@ -29,10 +38,15 @@ export default AddTodo;
 const styles = StyleSheet.create({
   container: {
     padding: 60,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   todoInput: {
+    width: "100%",
     borderColor: "gray",
     borderWidth: 1,
+    paddingLeft: 8
   },
   actionButton: {
     flexDirection: "row",
@@ -41,5 +55,6 @@ const styles = StyleSheet.create({
   },
   btn: {
     width: "40%",
+    margin: 5,
   },
 });
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, ScrollView, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Button,
+} from "react-native";
 import AddTodo from "./AddTodo";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
-  const addTodo = (todo) => {
+  const [isVisible, setIsModalVisible] = useState(true);
+  const addTodo = (todo, setTodoText) => {
     setTodos((currentTodos) => [
       ...currentTodos,
       { id: Math.random().toString(), text: todo },
     ]);
+    setTodoText("");
+    setIsModalVisible(false);
   };
+  const addNewTodo = () => {
+    setIsModalVisible(true)
+  }
   return (
-    <View>
-      <AddTodo addTodoItem={addTodo} />
+    <View style={styles.addBtn}> 
+      <AddTodo addTodoItem={addTodo} isVisible={isVisible} setModalVisible={setIsModalVisible} />
+      <View>
+        <Button title="Add" onPress={addNewTodo} />
+      </View>
       <FlatList
+        style={styles.todos}
         data={todos}
         renderItem={({ item }) => (
-          <View style={styles.todo}>
-            <Text style={styles.todoText}>{item.text}</Text>
-          </View>
+          <TouchableOpacity>
+            <View style={styles.todo}>
+              <Text style={styles.todoText}>{item.text}</Text>
+            </View>
+          </TouchableOpacity>
         )}
         keyExtractor={(item) => item.id}
       />
@@ -29,7 +48,9 @@ const TodoList = () => {
 export default TodoList;
 
 const styles = StyleSheet.create({
-  todos: {},
+  todos: {
+    marginTop: 10,
+  },
   todo: {
     backgroundColor: "gray",
     margin: 10,
@@ -40,5 +61,8 @@ const styles = StyleSheet.create({
   },
   todoText: {
     color: "white",
+  },
+  addBtn: {
+    margin: 60,
   },
 });
